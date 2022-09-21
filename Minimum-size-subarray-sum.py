e8396a997f0237c3ffd2fab48dc772d7cdc5b149
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2022-09-21 20:58:59 +0300

    Create Minimum-size-subarray-sum.py

---
+++ Minimum-size-subarray-sum.py
@@ -0,0 +1,20 @@
+# Problem: https://leetcode.com/problems/minimum-size-subarray-sum
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        length = len(nums)+1
+        left_ptr = 0
+        prefix_sum = 0
+        
+        if target in nums:
+            return 1
+        
+        for i,val in enumerate(nums):
+            prefix_sum += val
+            while prefix_sum >= target:
+                length = min(length, i-left_ptr+1)
+                prefix_sum -= nums[left_ptr]
+                left_ptr += 1
+            
+        return length if length != len(nums)+1 else 0
+                
