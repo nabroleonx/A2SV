8d80b654b90901a446616559d9a86fea5fdbdcf4
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2022-10-11 13:50:30 +0300

    Update CountingSort1.py

--- CountingSort1.py
+++ CountingSort1.py
@@ -1,7 +1,7 @@
 # Problem: https://www.hackerrank.com/challenges/countingsort1
 
 def countingSort(arr):
-    result = [0]*len(arr)
+    result = [0]*100
     for i in arr:
         result[i] += 1
     
