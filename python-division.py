b83a088b956f2522f29506142e8783af56360c08
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-01-02 19:37:47 +0300

    Create python-division.py

---
+++ python-division.py
@@ -0,0 +1,8 @@
+# Problem: https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    print(a/b)
