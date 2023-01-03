53556ee9ab63beef2bae9b576c7fbfa6e6214217
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-01-03 21:47:29 +0300

    Create smallest-even-multiple.py

---
+++ smallest-even-multiple.py
@@ -0,0 +1,7 @@
+# Problem: https://leetcode.com/problems/smallest-even-multiple
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n%2 == 0:
+            return n
+        return 2*n
