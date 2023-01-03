d743c5439fff9ad2f80e6c05d4fa0d01b4ccb4b8
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-01-03 21:51:45 +0300

    Create py-if-else.py

---
+++ py-if-else.py
@@ -0,0 +1,19 @@
+# Problem: https://www.hackerrank.com/challenges/py-if-else
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2!=0:
+        print("Weird")
+    else:
+        if n in range(6,21):
+            print("Weird")
+        else:
+            print("Not Weird")
