e9cabc5d7d856f7ad2682a3889f8bdd7a6d86d24
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-01-03 22:07:00 +0300

    Create python-power-mod-power.py

---
+++ python-power-mod-power.py
@@ -0,0 +1,12 @@
+# Problem: https://www.hackerrank.com/challenges/python-power-mod-power
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+power = a**b
+mod = power % m 
+
+print(power)
+print(mod)
+ 
