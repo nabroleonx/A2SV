f4c59d17023518001e8cf9473c62b67b7b171448
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-05-04 12:11:53 +0300

    Time: 4681 ms (36.73%), Space: 70.1 MB (32.50%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,18 +3,19 @@ class Solution:
         if n <= 2:
             return 0
         
-        is_prime = [True]*n
+        is_prime = [True for _ in range(n+1)]
         
         is_prime[0] = False
         is_prime[1] = False
         
         i = 2
-        # res = n-2
-        
+        res = n-2
         for i in range(2, int(math.sqrt(n))+1):
             if is_prime[i]:
                 for j in range(i*i, n, i):
+                    if is_prime[j]:
+                        res -= 1
                     is_prime[j] = False
-                    # res -= 1
+            i += 1
         
-        return sum(is_prime)
+        return res
