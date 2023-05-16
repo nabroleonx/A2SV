b6a2218f5c4f578de67f05062a5d3c35b11bc85f
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-05-16 23:43:16 +0300

    Update Regular_graph.py

--- Regular_graph.py
+++ Regular_graph.py
@@ -1,13 +1,11 @@
 n, m = map(int, input().split())
-degree = [0] * n
+degrees = [0] * (n + 1)
 for i in range(m):
     u, v = map(int, input().split())
-    u -= 1
-    v -= 1
-    degree[u] += 1
-    degree[v] += 1
-degree.sort()
-if degree[0] == degree[-1]:
+    degrees[u] += 1
+    degrees[v] += 1
+degrees.sort()
+if degrees[-1] == degrees[1]:
     print("YES")
 else:
     print("NO")
