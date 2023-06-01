9a393b46ddc25e4ad0c8c727bbf6f8e3eea42dbf
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-06-01 15:22:33 +0300

    Time: 694 ms (39.91%), Space: 18.1 MB (9.53%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,31 +1,30 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        queue = deque()
-        queue.append(['0000', 0])
+        queue = deque([('0000', 0)])
         visited = set(deadends)
         
         if '0000' in visited:
             return -1
         
-        def possibilities(code):
-            res = []
-            for i in range(4):
-                turned = str((int(code[i]) + 1) % 10)
-                res.append(code[:i]+turned+code[i+1:])
-            
-                turned = str((int(code[i]) - 1 + 10) % 10)
-                res.append(code[:i]+turned+code[i+1:])
-            return res
-        
         while queue:
-            code, move = queue.popleft()
+            code, moves = queue.popleft()
             
             if code == target:
-                    return move
-                
-            for i in possibilities(code):                
-                if i not in visited:
-                    visited.add(i)
-                    queue.append([i, move+1])
-                
+                return moves
+            
+            if code in visited:
+                continue
+            
+            visited.add(code)
+            
+            for i in range(4):
+                digit = int(code[i])
+                for d in [-1, 1]:
+                    turned = (digit + d) % 10
+                    new_code = code[:i] + str(turned) + code[i+1:]
+                    
+                    if new_code not in visited:
+                        queue.append((new_code, moves + 1))
+        
         return -1
+
