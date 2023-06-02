df166317349d5f71fe49e04a7185b3198bd31c30
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-06-02 23:58:42 +0300

    Time: 1493 ms (21.38%), Space: 17 MB (15.59%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,29 +1,36 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        graph = collections.defaultdict(list)
-        n = len(bombs)
+        edge = defaultdict(list)
+        def euclid(b1, b2):
+            a, b, c = b1
+            x, y, z = b2
+            
+            dist = math.sqrt((x-a)**2 + (y-b)**2)
+            
+            return dist <= c
         
+        n = len(bombs)
         for i in range(n):
             for j in range(n):
-                xi, yi, ri = bombs[i]
-                xj, yj, _ = bombs[j]
-                
-                if ri ** 2 >= (xi - xj) ** 2 + (yi - yj) ** 2:
-                    graph[i].append(j)
+                if i == j:
+                    continue
+                if euclid(bombs[i], bombs[j]):
+                    edge[i].append(j)
+                    
+        def dfs(i, visited):
+            if i in visited:
+                return 0
+            cur = 1
+            visited.add(i)
+            for j in edge[i]:
+                cur += dfs(j, visited)
+            
+            return cur
         
-        def dfs(i):
-            stack = [i]
-            visited = set([i])
-            while stack:
-                cur = stack.pop()
-                for neib in graph[cur]:
-                    if neib not in visited:
-                        visited.add(neib)
-                        stack.append(neib)
-            return len(visited)
-        
-        answer = 0
+        ans = 0
         for i in range(n):
-            answer = max(answer, dfs(i))
+            visited = set()
+            if i not in visited:
+                ans = max(ans, dfs(i, visited))
         
-        return answer
+        return ans
