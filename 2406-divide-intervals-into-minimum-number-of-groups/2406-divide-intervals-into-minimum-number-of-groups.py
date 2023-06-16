862c01c59179384e364187249044a02d3a765ed3
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2023-06-16 09:43:03 +0300

    Time: 9027 ms (5.55%), Space: 79.6 MB (6.67%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,5 +1,6 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
+        '''
         intervals.sort()
         heap = []
         
@@ -12,3 +13,21 @@ class Solution:
             heapq.heappush(heap, end)
         
         return len(heap)
+        '''
+        # alternative solution using prefix sum
+        max_end = 10**6 + 2
+        count = [0 for _ in range(max_end)]
+        
+        for interval in intervals:
+            start, end = interval
+            
+            count[start] += 1
+            count[end+1] -= 1
+        
+        groups = float('-inf')
+        
+        for i in range(1, len(count)):
+            count[i] += count[i-1]
+            groups = max(groups, count[i])
+            
+        return groups
